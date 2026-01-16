a46fe95b3743ca39b8e6940dd2d33d5e51d7f1cf
kh296 <kh296@users.noreply.github.com>
2026-01-16 00:58:35 +0000

    Updated install script

--- scripts/install_for_workshop.sh
+++ scripts/install_for_workshop.sh
@@ -15,23 +15,23 @@ WORKSHOP_HOME=$(cd $(dirname "$0")/..; pwd)
 if [[ ${WORKSHOP_HOME} == /var/spool/* ]]; then
     WORKSHOP_HOME=$(dirname $(pwd))
 fi
-USER_RDS=${HOME}/rds/rds-aswsai/260116-dawn-workshop
-rm -rf ${USER_RDS}
-mkdir -p ${USER_RDS}
+WORKSHOP_RDS=${HOME}/rds/rds-aswsai/260116-dawn-workshop
+rm -rf ${WORKSHOP_RDS}
+mkdir -p ${WORKSHOP_RDS}
 
 SCRIPTS_HOME=${WORKSHOP_HOME}/scripts
-INSTALL_RDS=${USER_RDS}/install
-mkdir -p ${INSTALL_RDS}
-INSTALL_SCRIPTS="miniforge3_install.sh diffusion-models_install.sh"
+SCRIPTS_RDS=${WORKSHOP_RDS}/scripts
+mkdir -p ${SCRIPTS_RDS}
+INSTALL_SCRIPTS="miniforge3_install.sh practical-ml-with-pytorch_install.sh ai_install.sh"
 for INSTALL_SCRIPT in ${INSTALL_SCRIPTS}; do
-    cp ${SCRIPTS_HOME}/${INSTALL_SCRIPT}_install.sh ${INSTALL_RDS}
+    cp ${SCRIPTS_HOME}/${INSTALL_SCRIPT} ${SCRIPTS_RDS}
 done
 
 # Link default user directory for Jupyter kernels
-# to user sub-directory of rds-aswsai,
+# to user sub-directory of rds-rsecon,
 # deleting any pre-existing kernels.
 JUPYTER_KERNELS_HOME="${HOME}/.local/share/jupyter/kernels"
-JUPYTER_KERNELS_RDS="${USER_RDS}/jupyter/kernels"
+JUPYTER_KERNELS_RDS="${WORKSHOP_RDS}/jupyter/kernels"
 
 rm -rf ${JUPYTER_KERNELS_HOME}
 mkdir -p $( dirname ${JUPYTER_KERNELS_HOME})
@@ -40,14 +40,14 @@ rm -rf ${JUPYTER_KERNELS_RDS}
 mkdir -p ${JUPYTER_KERNELS_RDS}
 ln -s ${JUPYTER_KERNELS_RDS} ${JUPYTER_KERNELS_HOME}
 
-cd ${INSTALL_RDS}
+cd ${SCRIPTS_RDS}
 for INSTALL_SCRIPT in ${INSTALL_SCRIPTS}; do
     echo ""
     ./${INSTALL_SCRIPT}
 done
 
 # Allow group access to installation.
-CMD="chmod -R g=u-w ${USER_RDS}"
+CMD="chmod -R g=u-w ${WORKSHOP_RDS}"
 echo ""
 echo "Setting group permissions:"
 echo "${CMD}"
